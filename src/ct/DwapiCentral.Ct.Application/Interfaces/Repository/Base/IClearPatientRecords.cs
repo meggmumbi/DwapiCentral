bbe4c1232ab620935f2730245cc3666ef54822b8
meggmumbi <margaretmumbi16@gmail.com>
2023-05-09 15:18:43 +0300

    Revamp

--- src/ct/DwapiCentral.Ct.Application/Interfaces/Repository/Base/IClearPatientRecords.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DwapiCentral.Ct.Application.Interfaces.Repository.Base
-{
-    public interface IClearPatientRecords
-    {
-        void Clear(Guid patientId);
-    }
-}
