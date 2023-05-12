480cc6f471f34129271bc82d2a313cd818bdfc9d
meggmumbi <margaretmumbi16@gmail.com>
2023-05-12 11:01:56 +0300

    Ct service

--- src/ct/DwapiCentral.Ct.Application/Services/PatientService.cs
+++ src/ct/DwapiCentral.Ct.Application/Services/PatientService.cs
@@ -3,6 +3,8 @@ using DwapiCentral.Ct.Application.Interfaces.Repository;
 using DwapiCentral.Ct.Application.Interfaces.Services;
 using DwapiCentral.Ct.Domain.Models.Extracts;
 using DwapiCentral.Shared.Domain.Model.Prep;
+using log4net.Core;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +19,14 @@ namespace DwapiCentral.Ct.Application.Services
         private readonly IPatientExtractRepository _patientRepository;
         private readonly IFacilityRepository    _facilityRepository;
         private readonly IManifestRepository _manifestRepository;
+        private readonly ILogger<PatientService> _logger;
         
-        public PatientService(IPatientExtractRepository patientRepository, IFacilityRepository facilityRepository, IManifestRepository manifest)
+        public PatientService(IPatientExtractRepository patientRepository, IFacilityRepository facilityRepository, IManifestRepository manifest,ILogger<PatientService> logger)
         {
             _patientRepository = patientRepository;
             _facilityRepository = facilityRepository;
             _manifestRepository = manifest;
+            _logger = logger;
         }
 
         public async Task AddManifestAsync(Manifest manifest)
@@ -44,35 +48,50 @@ namespace DwapiCentral.Ct.Application.Services
 
         public async void ProcessPatientData(IEnumerable<PatientExtract> patients)
         {
-            //var distinctPatients = patients
-            //    .GroupBy(p => p.PatientPID)
-            //    .Select(g => g.First());
-
-            //foreach (var patientDTO in distinctPatients)
-            //{
-            //    var exists = await _patientRepository.ExistsAsync(patientDTO.PatientPID);
-
-            //    if (exists)
-            //    {
-            //        throw new Exception($"");
-            //    }
-
-            //    var patient = new PatientExtract
-            //    {
-            //        PatientPID = patientDTO.PatientPID
-
-            //    };
-
-            //    var uniquePatients = new HashSet<PatientExtract>(new PatientComparer());
-            //foreach (var patient in patients)
-            //{
-            //    uniquePatients.Add(patient);
-            //}
+            try
+            {
 
 
-           
+                //var distinctPatients = patients
+                //    .GroupBy(p => p.PatientPID)
+                //    .Select(g => g.First());
+
+                //foreach (var patientDTO in distinctPatients)
+                //{
+                //    var exists = await _patientRepository.ExistsAsync(patientDTO.PatientPID);
+
+                //    if (exists)
+                //    {
+                //        throw new Exception($"");
+                //    }
+
+                //    var patient = new PatientExtract
+                //    {
+                //        PatientPID = patientDTO.PatientPID
+
+                //    };
+
+                //    var uniquePatients = new HashSet<PatientExtract>(new PatientComparer());
+                //foreach (var patient in patients)
+                //{
+                //    uniquePatients.Add(patient);
+                //}
+
+                _logger.LogInformation("Start saving...");
+
+                //_logger.LogInformation($"Patients sent {patients.Count} patients");
+
 
                 await _patientRepository.AddPatientsAsync(patients);
+
+                _logger.LogInformation("data saved successfully");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to save data");
+
+                throw;
+            }
             
         }
     }
